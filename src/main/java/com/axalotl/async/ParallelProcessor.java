8fc2d6b2242356798367897d85abb01fbd6fe287
Arif <68075877+devAgant@users.noreply.github.com>
2025-06-20 19:27:19 -0500

    Fix async tick locks

--- src/main/java/com/axalotl/async/ParallelProcessor.java
+++ src/main/java/com/axalotl/async/ParallelProcessor.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 import net.minecraft.Bootstrap;
 import net.minecraft.entity.*;
 import net.minecraft.entity.projectile.ProjectileEntity;
-import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.vehicle.*;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.dedicated.MinecraftDedicatedServer;
@@ -105,7 +104,6 @@ public class ParallelProcessor {
                 entity instanceof ProjectileEntity ||
                 entity instanceof AbstractMinecartEntity ||
                 entity instanceof ServerPlayerEntity ||
-                entity instanceof MobEntity ||
                 specialEntities.contains(entity.getClass()) ||
                 blacklistedEntity.contains(entityId) ||
                 AsyncConfig.synchronizedEntities.contains(EntityType.getId(entity.getType())) ||
